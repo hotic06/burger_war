f80c57506c01f1d0479f2930b73680edf21349b5
Ubuntu <ubuntu@ip-172-31-43-143.ap-northeast-1.compute.internal>
2019-08-12 15:13:42 +0000

    Window position automatic setting scripts

---
+++ scripts/set_window_pos.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+TERMINALS=`xdotool search -onlyvisible gnome-terminal`
+for WINID in ${TERMINALS} ; do
+ xdotool windowminimize ${WINID}
+done
+
+GAZEBOWIN=`xdotool search -onlyvisible Gazebo`
+xdotool windowactivate ${GAZEBOWIN}
+xdotool windowsize ${GAZEBOWIN} 1000 700
+xdotool windowmove ${GAZEBOWIN} 10 10
+sleep 0.2
+xdotool mousemove 900 70
+xdotool click 1
+sleep 0.2
+xdotool mousemove_relative 50 30 
+xdotool click 1
+sleep 1.5
+xdotool click 1
+sleep 1.0
+xdotool mousemove 630 380
+sleep 0.2
+xdotool click 1
+sleep 0.2
+xdotool click 1
+sleep 0.5
+xdotool click 4
+sleep 0.4
+xdotool click 4
+sleep 0.4
+
+
+xdotool windowsize ${GAZEBOWIN} 85% 100%
+xdotool windowmove ${GAZEBOWIN} 130 10
+
+JUDGE=`xdotool search -onlyvisible Onigiri`
+xdotool windowactivate ${JUDGE}
+xdotool windowmove ${JUDGE} 0 10
+
+
---
+++ scripts/set_window_pos_2.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+DESKTOPWIDTH=`xdpyinfo  | grep -oP 'dimensions:\s+\K\S+' | cut -f 1 -d "x"`
+DESKTOPHEIGHT=`xdpyinfo  | grep -oP 'dimensions:\s+\K\S+' | cut -f 2 -d "x"`
+
+
+RVIZWIN1=`xdotool search -onlyvisible ".*red.*Rviz"`
+
+if [ -n $RVIZWIN1 ]; then
+xdotool windowactivate ${RVIZWIN1}
+xdotool windowmove ${RVIZWIN1} 0 10
+xdotool mousemove 377 500
+sleep 0.2
+xdotool click 1
+sleep 0.2
+RVIZSIZE=400
+xdotool windowsize ${RVIZWIN1} ${RVIZSIZE} ${RVIZSIZE}
+xdotool windowmove ${RVIZWIN1} `expr ${DESKTOPWIDTH} - ${RVIZSIZE}` 10
+fi
+
+RVIZWIN2=`xdotool search -onlyvisible ".*blue.*Rviz"`
+
+if [ -n $RVIZWIN2 ]; then
+xdotool windowactivate ${RVIZWIN2}
+xdotool windowmove ${RVIZWIN2} 0 10
+xdotool mousemove 377 500
+sleep 0.2
+xdotool click 1
+sleep 0.2
+xdotool windowsize ${RVIZWIN2} ${RVIZSIZE} ${RVIZSIZE}
+xdotool windowmove ${RVIZWIN2} 10 320
+fi
+
+GAZEBOWIN=`xdotool search -onlyvisible Gazebo`
+xdotool windowactivate ${GAZEBOWIN}
+
+JUDGE=`xdotool search -onlyvisible Onigiri`
+xdotool windowactivate ${JUDGE}
+
+if [ -n $RVIZWIN1 ]; then
+xdotool windowactivate ${RVIZWIN1}
+fi
+if [ -n $RVIZWIN2 ]; then
+xdotool windowactivate ${RVIZWIN2}
+fi
+TERMINALS=`xdotool search -onlyvisible gnome-terminal`
+for WINID in ${TERMINALS} ; do
+ xdotool windowminimize ${WINID}
+done
